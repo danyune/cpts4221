413e4605ad6752c9df35c49f458363528780dfda
Dan Yune <danyune@gmail.com>
2019-09-21 02:07:57 -0700

    not sure wtf i deleted and stuff

--- FirstChecks/src/main/OperatorsCheck.java
+++ FirstChecks/src/main/OperatorsCheck.java
@@ -3,7 +3,6 @@ package main;
 import com.puppycrawl.tools.checkstyle.api.*;
 
 public class OperatorsCheck extends AbstractCheck {
-	private static final String CATCH_MSG = "GENERIC CATCH MESSAGE: ";
 	private int operators = 0;
 
 	@Override
@@ -11,27 +10,63 @@ public class OperatorsCheck extends AbstractCheck {
 		return new int[0];
 	}
 
+//	@Override
+//	public int[] getAcceptableTokens() {
+//		// TODO Auto-generated method stub
+//		return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+//	}
+
 	@Override
 	public int[] getAcceptableTokens() {
 		// TODO Auto-generated method stub
-		return new int[0];
+		return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
 	}
 
 	@Override
 	public int[] getDefaultTokens() {
-		return new int[] { TokenTypes.ELIST };
+		return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+	}
+
+	@Override
+	public void beginTree(DetailAST ast) {
+		operators = 0;
 	}
 
 	@Override
 	public void finishTree(DetailAST ast) {
-		log(0, CATCH_MSG + operators);
+		// Edit what you want the warning area to display
+		String catchMsg = "PLACEHOLDER: ";
+		log(ast.getLineNo(), catchMsg + operators);
 	}
 
 	@Override
 	public void visitToken(DetailAST ast) {
-	    DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
-	    DetailAST block2 = objBlock.findFirstToken(TokenTypes.METHOD_DEF);
-		operators += block2.getChildCount(TokenTypes.ELIST);
+		DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
+		
+		// Put whatever checks here, not sure how to make it a list
+		operators += countTokens(objBlock, TokenTypes.ASSIGN);
+		operators += countTokens(objBlock, TokenTypes.PLUS);
 	}
 
+	private int countTokens(DetailAST ast, int tokenTypes) {
+		// If the class has anything in it first
+		if (ast.getChildCount() > 0) {
+			int count = 0;
+			count += ast.getChildCount(tokenTypes);
+			
+			// Find first child, assuming first method
+			DetailAST child = ast.getFirstChild();
+			
+			// Keep checking each method until we have no more
+			while (child != null) {
+				count += countTokens(child, tokenTypes);
+				child = child.getNextSibling();
+			}
+			return count;
+			
+		// Means class didn't have nothin
+		} else {
+			return 0;
+		}
+	}
 }
