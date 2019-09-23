b0f6a9fd7ebcc2c68786288d6ba6919217a7e226
Dan Yune <danyune@gmail.com>
2019-09-23 00:21:02 -0700

    trying to get operands to work. not sure if its right because reusing variables doesnt add count, such as temp = temp + temp

---
+++ FirstChecks/src/main/OperandCheck.java
@@ -0,0 +1,76 @@
+package main;
+
+import java.util.Arrays;
+
+import com.puppycrawl.tools.checkstyle.api.*;
+
+public class OperandCheck extends AbstractCheck {
+	private int operandCount = 0;
+
+	@Override
+	public int[] getRequiredTokens() {
+		return new int[0];
+	}
+
+	@Override
+	public int[] getAcceptableTokens() {
+		return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+	}
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] { TokenTypes.CLASS_DEF, TokenTypes.INTERFACE_DEF };
+	}
+
+	@Override
+	public void beginTree(DetailAST ast) {
+		operandCount = 0;
+	}
+
+	@Override
+	public void finishTree(DetailAST ast) {
+		String catchMsg = "Number of Operands: ";
+		log(ast.getLineNo(), catchMsg + operandCount);
+	}
+
+	@Override
+	public void visitToken(DetailAST ast) {
+		DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
+		Arrays.stream(tokenTypes()).forEach(x -> operandCount += countTokens(objBlock, x));
+	}
+
+	private int[] tokenTypes() {
+		return new int[] { TokenTypes.EXPR };
+	}
+
+	private int[] operandArray() {
+		return new int[] { TokenTypes.NUM_DOUBLE, TokenTypes.NUM_FLOAT, TokenTypes.NUM_INT, TokenTypes.NUM_LONG,
+				TokenTypes.VARIABLE_DEF };
+	}
+
+	private int countTokens(DetailAST ast, int tokenTypes) {
+		// If the class has anything in it first
+		if (ast.getChildCount() > 0) {
+			int count = 0;
+//			count += ast.getChildCount(tokenTypes);
+			int[] tokenArray = operandArray();
+			for (int token : tokenArray) {
+				count += ast.getChildCount(token);
+			}
+			
+			// Find first child, assuming first method
+			DetailAST child = ast.getFirstChild();
+
+			// Keep checking each method until we have no more
+			while (child != null) {
+				count += countTokens(child, tokenTypes);
+				child = child.getNextSibling();
+			}
+			return count;
+
+			// Means class/interface didn't have nothin
+		} else {
+			return 0;
+		}
+	}
+}
