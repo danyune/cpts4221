c8ae011709da1c121467ff99ddce4ef651a0db34
Dan Yune <danyune@gmail.com>
2019-09-27 17:39:29 -0700

    lmfao the halstead length is so inefficient fix it later

--- FirstChecks/src/main/HalsteadVocabularyCheck.java
+++ FirstChecks/src/main/HalsteadVocabularyCheck.java
@@ -6,7 +6,7 @@ import com.puppycrawl.tools.checkstyle.api.*;
 
 public class HalsteadVocabularyCheck extends AbstractCheck {
 	private int uniqueCount = 0;
-	private Set<String> variableList = new HashSet<String>();
+	private Set<String> variableSet = new HashSet<String>();
 
 	@Override
 	public int[] getRequiredTokens() {
@@ -36,8 +36,42 @@ public class HalsteadVocabularyCheck extends AbstractCheck {
 
 	@Override
 	public void visitToken(DetailAST ast) {
+
 		DetailAST objBlock = ast.findFirstToken(TokenTypes.OBJBLOCK);
-		uniqueCount += countTokens(objBlock);
+
+		if (objBlock.getChildCount() > 0) {
+
+			for (int n : operandArray()) {
+				if (objBlock.branchContains(n)) {
+					uniqueCount++;
+				}
+			}
+			
+			for (int n : operatorsArray()) {
+				if (objBlock.branchContains(n)) {
+					uniqueCount++;
+				}
+			}
+
+			getVariableNames(objBlock);
+			uniqueCount += variableSet.size();
+		}
+
+	}
+
+	private void getVariableNames(DetailAST ast) {
+		if (ast.getChildCount() > 0) {
+			if (ast.getType() == TokenTypes.VARIABLE_DEF) {
+				variableSet.add(ast.findFirstToken(TokenTypes.IDENT).getText());
+//				uniqueCount++;
+			}
+			DetailAST child = ast.getFirstChild();
+
+			while (child != null) {
+				getVariableNames(child);
+				child = child.getNextSibling();
+			}
+		}
 	}
 
 	private int[] operatorsArray() {
@@ -45,66 +79,8 @@ public class HalsteadVocabularyCheck extends AbstractCheck {
 				TokenTypes.MOD };
 	}
 
+	// Not including identity here
 	private int[] operandArray() {
-		return new int[] { TokenTypes.NUM_DOUBLE, TokenTypes.NUM_FLOAT, TokenTypes.NUM_INT, TokenTypes.NUM_LONG,
-				TokenTypes.IDENT };
-	}
-
-	private int countTokens(DetailAST ast) {
-		// If the class has anything in it first
-		if (ast.getChildCount() > 0) {
-			DetailAST child;
-			int count = 0;
-
-			if (ast.getChildCount(TokenTypes.VARIABLE_DEF) > 0) {
-				child = ast.findFirstToken(TokenTypes.VARIABLE_DEF);
-				if (child.getChildCount(TokenTypes.ASSIGN) > 0) {
-					count += countGoodSection(child);
-				}
-			}
-
-			if (ast.getChildCount(TokenTypes.EXPR) > 0) {
-				child = ast.findFirstToken(TokenTypes.EXPR);
-				while (child != null) {
-					if (child.getChildCount(TokenTypes.ASSIGN) > 0) {
-						count += countGoodSection(child);
-					}
-					child = child.getNextSibling();
-				}
-			}
-
-			// Find first child, assuming first method
-			child = ast.getFirstChild();
-
-			// Keep checking each method until we have no more
-			while (child != null) {
-				count += countTokens(child);
-				child = child.getNextSibling();
-			}
-			return count;
-
-			// Means class/interface didn't have nothin
-		} else {
-			return 0;
-		}
-	}
-
-	private int countGoodSection(DetailAST ast) {
-		if (ast.getChildCount() > 0) {
-			int total = 0;
-			for (int n : operandArray()) {
-				total += ast.getChildCount(n);
-			}
-
-			DetailAST child = ast.getFirstChild();
-
-			while (child != null) {
-				total += countGoodSection(child);
-				child = child.getNextSibling();
-			}
-			return total;
-		} else {
-			return 0;
-		}
+		return new int[] { TokenTypes.NUM_DOUBLE, TokenTypes.NUM_FLOAT, TokenTypes.NUM_INT, TokenTypes.NUM_LONG };
 	}
 }
