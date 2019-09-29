ad1a1c7536705b105d7ed84312ca1164c644ea39
Rebecca <rrothsch@eecs.wsu.edu>
2019-09-29 00:37:03 -0700

    Implemented total comment count check. Working on number of lines of comments

---
+++ FirstChecks/src/main/LinesOfCommentsCheck.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.util.Arrays;
+
+import com.puppycrawl.tools.checkstyle.api.*;
+
+public class LinesOfCommentsCheck extends AbstractCheck {
+	
+	private int commentLines = 0;
+
+	@Override
+	public int[] getRequiredTokens() {
+		return new int[0];
+	}
+	
+	@Override
+	public boolean isCommentNodesRequired() {
+		return true;
+	}
+
+	@Override
+	public int[] getAcceptableTokens() {
+		return new int[] { TokenTypes.COMMENT_CONTENT };
+	}
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] { TokenTypes.COMMENT_CONTENT };
+	}
+
+	@Override
+	public void beginTree(DetailAST ast) {
+		commentLines = 0;
+	}
+
+	@Override
+	public void finishTree(DetailAST ast) {
+		
+		String catchMsg = "Lines of Comments: ";
+		log(ast.getLineNo(), catchMsg + commentLines);
+	}
+
+	@Override
+	public void visitToken(DetailAST ast) {
+		DetailAST objBlock = ast.findFirstToken(TokenTypes.COMMENT_CONTENT);
+		Arrays.stream(tokenTypes()).forEach(x -> commentLines += countTokens(objBlock, x));
+	}
+
+	private int[] tokenTypes() {
+		return new int[] {TokenTypes.COMMENT_CONTENT};
+	}
+
+	private int countTokens(DetailAST ast, int tokenTypes) {
+
+		return 1;
+	}
+}
